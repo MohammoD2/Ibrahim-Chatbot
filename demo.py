97f255c167894c7b806ae3c822aada62449b43e0
MohammoD2 <ibr500037@gmail.com>
2025-11-14 04:33:57 +0600

    adding Personal Chatbot

--- demo.py
+++
@@ -1,8 +0,0 @@
-from Chatbot import chatbot
-
-
-while True:
-    user_input = input("Enter your text: ")
-    response = chatbot(user_input)
-    print(response)
-
--- test.py
+++ test.py
@@ -4,19 +4,20 @@ import json
 response = requests.post(
   url="https://openrouter.ai/api/v1/chat/completions",
   headers={
-    "Authorization": "Bearer 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",
-    "Content-Type": "application/json"
- # Optional. Site title for rankings on openrouter.ai.
+    "Authorization": "Bearer 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",
+    "Content-Type": "application/json",
+    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
   },
   data=json.dumps({
-    "model": "tngtech/deepseek-r1t2-chimera:free",
+    "model": "deepseek/deepseek-r1-0528-qwen3-8b:free",
     "messages": [
       {
         "role": "user",
-        "content": "What is the meaning of life?"
+        "content": "hello"
       }
     ],
     
   })
 )
-print(response.status_code, response.text)  
+print(response.json())
